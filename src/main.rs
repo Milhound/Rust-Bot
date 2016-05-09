ad2f29608fb821651014f3e1e989c29ded2b6ebd
Daniel <Daniel.Milholland@icloud.com>
2016-05-09 15:08:55 -0500

    Updated to add cat

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,69 @@
 extern crate discord;
+extern crate hyper;
+extern crate serde_json;
 
 use discord::Discord;
 use discord::model::Event;
-
 use std::env;
+use std::error::Error;
+
+fn get_cat() -> Result<String, Box<Error>> {
+    let url: &str = "http://random.cat/meow";
+    use std::io::Read;
+    use serde_json::Value;
+
+    let client = hyper::Client::new();
+    let mut response = try!(client.get(url).send());
+    let mut buff = String::new();
+    try!(response.read_to_string(&mut buff));
+    let decode: Value = try!(serde_json::from_str(&buff));
+    let data = decode.as_object().expect("Invalid JSON");
+    let file = data.get("file").expect("File not found");
+    Ok(file.as_string().unwrap().to_string())
+}
 
 fn main() {
-	// Log in to Discord using the email and password in the environment
-	let discord = Discord::from_bot_token(&env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN")).expect("Discord Token Error");
-
-	// Establish and use a websocket connection
-	let (mut connection, _) = discord.connect().expect("Connection failed.");
+    let discord = Discord::from_bot_token(&env::var("RUST_BOT_TOKEN").expect("DISCORD TOKEN")).expect("Discord Token Error");
+    println!("{:?}", &env::var("RUST_BOT_TOKEN"));
+	let (mut connection, _) = discord.connect().expect("Connection Failed.");
 	println!("Ready...");
-	loop {
+
+    loop {
 		match connection.recv_event() {
-		/*
 			Ok(Event::MessageCreate(message)) => {
-				pub struct Message {
-			    pub id: MessageId,
-			    pub channel_id: ChannelId,
-			    pub content: String,
-			    pub nonce: Option<String>,
-			    pub tts: bool,
-			    pub timestamp: String,
-			    pub edited_timestamp: Option<String>,
-			    pub author: User,
-			    pub mention_everyone: bool,
-			    pub mentions: Vec<User>,
-			    pub attachments: Vec<Attachment>,
-			    pub embeds: Vec<Value>,
-			}
-		*/
-			
 				println!("{} says: {}", message.author.name, message.content);
+                match message.content.as_ref() {
 
-				match message.content.as_ref() { 
-					//fn send_message(&self, channel: &ChannelId, text: &str, nonce: &str, tts: bool) -> Result<Message>
-					"!ping" => { 
-					// Format! used to convert (String::collections, &str) to simple &str
-					//{:?} used to unwrap tuple over {} for literal
-					//USER_ID = pub struct UserId(pub u64);
-					//Use .0 to access the first value of a struct like an Array
-					
-					// Pong returns @user, Pong
-					let pong = format!("<@{:?}>, Pong", &message.author.id.0);
-					let _ = discord.send_message(&message.channel_id, &pong , "", false);},
-					// Info returns Rust bot was programmed in Rust Lang, using Discord-rs: https://github.com/SpaceManiac/discord-rs.
-					"/info" => { let _ = discord.send_message(&message.channel_id, "Rust bot was programmed in Rust Lang, using Discord-rs: https://github.com/SpaceManiac/discord-rs.", "", false);},
-    				// Test returns This is a reply to the test.
-    				"/test" => { let _ = discord.send_message(&message.channel_id, "This is a reply to the test.", "", false); },
-					// Help returns If your seeking help from this bot you may not find it.
-					"/help" => { let _ = discord.send_message(&message.channel_id, "If your seeking help from this bot you may not find it.", "", false); },
-					// Force Closes the bot
-					"/quit" => {println!("Quitting..."); break},
-					_ => continue,
-					
-				}
-
+                    "!cat" => {
+                        if let Ok(s) = get_cat() {
+                            println!("{}", s);
+                        }
+                    },
+                    "!ping" => {
+                        let pong = format!("<@{:?}>, Pong", &message.author.id.0);
+                        let _ = discord.send_message(&message.channel_id, &pong , "", false);
+                    },
+                    "/info" => { let _ = discord.send_message(&message.channel_id,
+                        "Rust bot was programmed in Rust Lang, using Discord-rs: https://github.com/SpaceManiac/discord-rs.", "", false);
+                    },
+                    "/test" => { let _ = discord.send_message(&message.channel_id,
+                        "This is a reply to the test.", "", false);
+                    },
+                    "/help" => { let _ = discord.send_message(&message.channel_id,
+                        "If your seeking help from this bot you may not find it.", "", false);
+                    },
+                    "/quit" => {println!("Quitting..."); break},
+                        _ => continue,
+                }
 			}
 			Ok(_) => {}
-
+			Err(discord::Error::Closed(code, body)) => {
+				println!("Gateway closed on us with code {:?}: {}", code, String::from_utf8_lossy(&body));
+				break
+			}
 			Err(err) => println!("Receive error: {:?}", err)
 		}
 	}
-
 	// Log out from the API
 	discord.logout().expect("logout failed");
 }
-
-
